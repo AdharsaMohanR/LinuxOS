e3c69c708b6c4d3e8e0a520cab635b8dfb43761d
adharsa mohan r <69114229+AdharsaMohanR@users.noreply.github.com>
2021-03-03 11:42:23 +0530

    Update systcall2.c

--- Assignment2/systcall2.c
+++ Assignment2/systcall2.c
@@ -4,22 +4,14 @@
 int main()
 {
     FILE * f;
-    char path[100];
-
-    char ch;
+    char path[70],ch;
     int chars, words, lines;
-
-
-    /* Input path of files to merge to third file */
     printf("Enter source file path: ");
     scanf("%s", path);
-
-    /* Open source files in 'r' mode */
     file = fopen(path, "r");
-    /* Check if file opened successfully */
     if (f == NULL)
     {
-        printf("\nUnable to open file.\n");
+        printf("Not able  to open file.\n");
         printf(" check if file exists and  have read privilege.\n");
 
         exit(EXIT_FAILURE);
@@ -32,12 +24,8 @@ int main()
     while ((ch = fgetc(file)) != EOF)
     {
         chars++;
-
-        /* Check new line */
         if (ch == '\n' || ch == '\0')
             lines++;
-
-        /* Check words */
         if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
             words++;
     }
@@ -47,12 +35,10 @@ int main()
         words++;
         lines++;
     }
-
-    /* Print file statistics */
     printf("\n");
-    printf("Total characters = %d\n", chars);
-    printf("Total words      = %d\n", words);
-    printf("Total lines      = %d\n", lines);
+    printf("characters = %d\n", chars);
+    printf(" words      = %d\n", words);
+    printf("lines      = %d\n", lines);
 
 
     /* Close files to release resources */
