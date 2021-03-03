5e3ed763e8187ddb2f06a56702ff12262469d2ef
adharsa mohan r <69114229+AdharsaMohanR@users.noreply.github.com>
2021-03-03 23:11:39 +0530

    Update systcall3.c

--- Assignment2/systcall3.c
+++ Assignment2/systcall3.c
@@ -8,7 +8,50 @@
 void sighup(); 
 void sigint(); 
 void sigquit(); 
+void main() 
+{ 
+    int pid; 
+    if ((pid = fork()) < 0) { 
+        perror("fork"); 
+        exit(1); 
+    } 
   
-// driver code 
+    if (pid == 0) { 
+        signal(SIGHUP, sighup); 
+        signal(SIGINT, sigint); 
+        signal(SIGQUIT, sigquit); 
+        for (;;) 
+            ; 
+    } 
+  else 
+    { 
+        printf("\nPARENT: sending SIGHUP\n\n"); 
+        kill(pid, SIGHUP); 
+  
+        sleep(3); 
+        printf("\nPARENT: sending SIGINT\n\n"); 
+        kill(pid, SIGINT); 
+  
+        sleep(3);
+        printf("\nPARENT: sending SIGQUIT\n\n"); 
+        kill(pid, SIGQUIT); 
+        sleep(3); 
+    } 
+} 
+void sighup() 
+ { 
+    signal(SIGHUP, sighup);
+    printf("CHILD:  received a SIGHUP\n"); 
+} 
+void sigint() 
+  { 
+    signal(SIGINT, sigint); 
+    printf("CHILD:  received a SIGINT\n"); 
+} 
+void sigquit() 
+{ 
+    printf("Child process is killed!!!\n"); 
+    exit(0); 
+} 
 
     
